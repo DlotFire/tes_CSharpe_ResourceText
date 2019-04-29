65ed4cc8cb632c6bd72923b96f32f057e56328bf
Dl <765560184@qq.com>
2019-04-29 09:51:32 +0800

    test LinQClass

--- tes_CSharpe_ResourceText/Program.cs
+++ tes_CSharpe_ResourceText/Program.cs
@@ -6,6 +6,8 @@ namespace tes_CSharpe_ResourceText
     {
         static void Main(string[] args)
         {
+            GetProjectPath();
+
             //TestConvertDotResource ts = new TestConvertDotResource();
 
             //new 网络.TestIPAdress();
@@ -20,7 +22,40 @@ namespace tes_CSharpe_ResourceText
 
             //new 反射.TestReflection_Assembly();
             //new 反射.TestReflection_module();
-            new 反射.TestReflection_Type();
+            //new 反射.TestReflection_Type();
+
+            new LINQ.TestIntroToLINQ();
+        }
+
+        private static void GetProjectPath()
+        {
+            //1.获取模块的完整路径。 
+            Console.WriteLine($"System.Diagnostics.Process.GetCurrentProcess():" +
+                $" \n{System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName}\n");
+
+            //2.获取和设置当前目录(该进程从中启动的目录)的完全限定目录 
+            Console.WriteLine($"Environment.CurrentDirectory: " +
+                $"\n{Environment.CurrentDirectory}\n");
+
+            //3.获取应用程序的当前工作目录 
+            Console.WriteLine($"System.IO.Directory.GetCurrentDirectory():" +
+                $" \n{System.IO.Directory.GetCurrentDirectory()}\n");
+
+            //4.获取程序的基目录 
+            Console.WriteLine($"AppDomain.CurrentDomain.BaseDirectory:" +
+                $" \n{AppDomain.CurrentDomain.BaseDirectory}\n");
+
+            //5.获取和设置包括该应用程序的目录的名称 
+            Console.WriteLine($"AppDomain.CurrentDomain.SetupInformation.ApplicationBase:" +
+                $" \n{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}\n");
+
+            //6.获取启动了应用程序的可执行文件的路径 
+            // System.Windows.Forms.Application.StartupPath;
+
+            //7.获取启动了应用程序的可执行文件的路径及文件名 
+            // System.Windows.Forms.Application.ExecutablePath;
+
+            Console.WriteLine("====================================================");
         }
     }
 }
